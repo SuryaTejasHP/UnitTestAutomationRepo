94154aa6e21b52236d5c33e87c96ec88450bac60
Surya Tejas <suryatejas.hp@gmail.com>
2026-01-06 16:54:34 +0530

    Add project files.

---
+++ UnitTestAutomation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UnitTestAutomation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
